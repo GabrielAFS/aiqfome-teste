8a4f4fb12474a8706902b15a2f96a650f924bbd2
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-05-08 11:22:49 -0300

    fix: Updates footer position and height

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className='fixed bottom-0 w-full bg-[var(--neutrals)] text-[var(--purple)] text-center font-bold py-6 px-4'>
+    <footer className='sticky bottom-0 w-full h-[119px] bg-[var(--neutrals)] text-[var(--purple)] text-center font-bold py-6 px-4'>
       <div className='flex flex-col items-center gap-2'>
         <p className='text-sm'>feito com 💜 em maringá-PR</p>
         <div className='text-[16px]'>
