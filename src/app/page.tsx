fc14d1834a74e197c95b0b5ca71b23a24e83fd9a
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-05-08 11:44:18 -0300

    feat: Creates card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Card from "@/components/card";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
@@ -14,7 +15,56 @@ export default function Home() {
         className='w-full h-32 object-cover'
         priority
       />
-      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'></main>
+      <main className='flex flex-col row-start-2 items-center px-4 py-6 overflow-y-scroll'>
+        <section className='w-full mb-6'>
+          <h1 className='text-xl font-bold text-[var(--primary)] mb-4'>
+            abertos
+          </h1>
+          <ul>
+            <li className='mb-4'>
+              <Card
+                title='Restaurante 1'
+                image='/matsuri.png'
+                deliveryFee={5.0}
+                rating={4.5}
+              />
+            </li>
+            <li className='mb-4'>
+              <Card
+                title='Restaurante 2'
+                image='/bk.png'
+                deliveryFee={0}
+                rating={4.5}
+              />
+            </li>
+          </ul>
+        </section>
+        <section className='w-full mb-6'>
+          <h1 className='text-xl font-bold text-[var(--primary)] mb-4'>
+            fechados
+          </h1>
+          <ul>
+            <li className='mb-4'>
+              <Card
+                title='Restaurante 1'
+                image='/matsuri.png'
+                deliveryFee={5.0}
+                rating={4.5}
+                isClosed
+              />
+            </li>
+            <li className='mb-4'>
+              <Card
+                title='Restaurante 2'
+                image='/bk.png'
+                deliveryFee={0}
+                rating={4.5}
+                isClosed
+              />
+            </li>
+          </ul>
+        </section>
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/card.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  title: string;
+  image: string;
+  deliveryFee: number;
+  rating: number;
+  isClosed?: boolean;
+}
+
+const Card: React.FC<Props> = ({
+  title,
+  image,
+  deliveryFee,
+  rating,
+  isClosed = false,
+}) => {
+  const isDeliveryForFree = deliveryFee === 0;
+  const formattedDeliveryFee = isDeliveryForFree
+    ? "grátis"
+    : deliveryFee.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+  const formattedRating = rating.toLocaleString("en", {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+    notation: "standard",
+    currencySign: "standard",
+  });
+
+  return (
+    <div className='w-full h-[72px] flex gap-2 bg-[var(--neutrals-light)] rounded-lg shadow-sm overflow-hidden'>
+      <div className='flex flex-1 items-center gap-2'>
+        <Image
+          src={image}
+          alt='Restaurant'
+          width={72}
+          height={72}
+          className={isClosed ? "opacity-40" : ""}
+        />
+        <div className='flex flex-col flex-1 p-4'>
+          <h2 className='text-md font-bold'>{title}</h2>
+          <div className='flex text-sm gap-2'>
+            <span
+              className={`flex items-center gap-1.5 text-[var(${
+                isDeliveryForFree ? "--teal" : "--purple"
+              })] font-bold`}
+            >
+              <Image
+                src={isDeliveryForFree ? "/motobike.svg" : "/colorful_logo.svg"}
+                alt='delivery'
+                width={18}
+                height={18}
+              />{" "}
+              <p>{formattedDeliveryFee}</p>
+            </span>
+            <span className='flex items-center gap-1.5'>
+              <Image src='/star.svg' alt='Star' width={18} height={18} />{" "}
+              <p className='text-[var(--text-light)]'>{formattedRating}</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
