84c02869e3f616f7e83fdd8bf8e9c3c6b12171b0
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-05-08 01:34:47 -0300

    feat: Adds banner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ export default function Home() {
   return (
     <div className='min-h-screen font-[family-name:var(--font-nunito)]'>
       <Header />
+      <Image
+        src='/banner.png'
+        alt='Banner'
+        width={1920}
+        height={1080}
+        className='w-full h-32 object-cover'
+        priority
+      />
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'></main>
       <Footer />
     </div>
